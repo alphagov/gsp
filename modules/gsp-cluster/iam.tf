d5c0b293c6ec9360011a1cd2290afbaad420282e
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-01-21 17:11:09 +0000

    Create a Dev policy
    This role can be assumed. This will not permit any  access to AWS resources.
    This role will be used for k8s authentication
    
    Co-Authored by Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
    Co-Authored by Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>

--- modules/gsp-cluster/iam.tf
+++ modules/gsp-cluster/iam.tf
@@ -18,3 +18,33 @@ resource "aws_iam_policy" "s3-user-data-policy" {
   name   = "${var.cluster_name}-s3-user-data-policy"
   policy = "${data.aws_iam_policy_document.user_data_policy_document.json}"
 }
+
+resource "aws_iam_role" "dev" {
+  name = "${var.cluster_name}-dev"
+
+  assume_role_policy = "${data.aws_iam_policy_document.grant-iam-dev.json}"
+}
+
+data "aws_iam_policy_document" "grant-iam-dev" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals = {
+      type = "AWS"
+      identifiers = ["${var.dev_user_arns}"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+      values   = ["${var.gds_external_cidrs}"]
+    }
+  }
+}
