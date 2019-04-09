9db86d61e3cb43a1142ba252f0ac3ad1d1e6cd0d
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2019-04-09 10:55:25 +0100

    Fix the policy document

--- modules/gsp-cluster/iam.tf
+++ modules/gsp-cluster/iam.tf
@@ -13,17 +13,7 @@ resource "aws_iam_role" "sre" {
 data "aws_iam_policy_document" "grant-iam-sre-policy" {
   statement {
     effect  = "Allow"
-    actions = [
-      "sts:AssumeRole",
-      "autoscaling:Describe*",
-      "cloudwatch:Describe*",
-      "cloudwatch:Get*",
-      "cloudwatch:List*",
-      "logs:Get*",
-      "logs:Describe*",
-      "sns:Get*",
-      "sns:List*"
-    ]
+    actions = ["sts:AssumeRole"]
 
     principals = {
       type = "AWS"
@@ -42,6 +32,36 @@ data "aws_iam_policy_document" "grant-iam-sre-policy" {
       variable = "aws:SourceIp"
       values   = ["${var.gds_external_cidrs}"]
     }
+  }
+}
+
+resource "aws_iam_policy_attachment" "cloudwatch-readonly" {
+  name       = "${var.cluster_name}-cloudwatch-readonly-attachment"
+  roles      = ["${aws_iam_role.sre.name}"]
+  policy_arn = "${aws_iam_policy.cloudwatch-readonly.arn}"
+}
+
+resource "aws_iam_policy" "cloudwatch-readonly" {
+  name        = "${var.cluster_name}-cloudwatch-readonly"
+
+  policy = "${data.aws_iam_policy_document.cloudwatch-readonly.json}"
+}
+
+data "aws_iam_policy_document" "cloudwatch-readonly" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "autoscaling:Describe*",
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*",
+      "logs:Get*",
+      "logs:Describe*",
+      "sns:Get*",
+      "sns:List*"
+    ]
+
+    resources = ["*"]
   }
 }
 
