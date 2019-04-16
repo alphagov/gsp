d7af9735cbf5634b94aaba661bd138f6583f60b7
Daniel Blair <daniel.blair@digital.cabinet-office.gov.uk>
2019-04-16 19:05:46 +0100

    Add control plane logging.

--- modules/k8s-cluster/main.tf
+++ modules/k8s-cluster/main.tf
@@ -11,6 +11,14 @@ resource "aws_eks_cluster" "eks-cluster" {
     subnet_ids         = ["${var.subnet_ids}"]
   }
 
+  enabled_cluster_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler",
+  ]
+
   depends_on = [
     "aws_iam_role_policy_attachment.eks-cluster-policy",
     "aws_iam_role_policy_attachment.eks-service-policy",
