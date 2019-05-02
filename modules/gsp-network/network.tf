be7e2e1edff5261ce3db9b8fc6845de5f0fca88a
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-02 17:04:32 +0100

    handcrafts and hardcodes the 10.0.0.0/16 subnets
    The naive subnet division wasted a lot of IP addresses and did not
    give us any room for future changes to the network layout (for instance
    if we wanted a subnet with different access/routing rules than simply
    "public" or "private")
    
    We have never made use of passing in a custom cidr range and it may be
    more confusing if we did, so instead we layout the subnets like this:
    
    ```
    10.0.0.0/16:
        10.0.0.0/18 — AZ A
            10.0.0.0/19 — Private (Pods, Nodes)
            10.0.32.0/19
                   10.0.32.0/20 — Public (ELBs, EKS)
                   10.0.48.0/20
                       10.0.48.0/21 — Not used yet
                       10.0.56.0/21 — Not used yet
        10.0.64.0/18 — AZ B
            10.0.64.0/19 — Private (Pods, Nodes)
            10.0.96.0/19
                    10.0.96.0/20 — Public (ELBs, EKS)
                    10.0.112.0/20
                        10.0.112.0/21 — Not used yet
                        10.0.120.0/21 — Not used yet
        10.0.128.0/18 — AZ C
            10.0.128.0/19 — Private (Pods, Nodes)
            10.0.160.0/19
                    10.0.160.0/20 — Public (ELBs, EKS)
                    10.0.176.0/20
                        10.0.176.0/21 — Not used yet
                        10.0.184.0/21 — Not used yet
        10.0.192.0/18 — Not used yet
    ```
    
    This gives 8192 Pod addresses per AZ and reserves 4096 addresses
    per AZ for use by things like load balancers, EKS and another other
    public facing things, and reserves 4096 address for unspecified future
    use.

--- modules/gsp-network/network.tf
+++ modules/gsp-network/network.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "network" {
-  cidr_block           = "${var.cidr_block}"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -15,37 +15,45 @@ resource "aws_internet_gateway" "gateway" {
   tags = "${map("Name", "${var.cluster_name}")}"
 }
 
-locals {
-  public_cidr_block  = "${cidrsubnet(aws_vpc.network.cidr_block, 1, 0)}"
-  private_cidr_block = "${cidrsubnet(aws_vpc.network.cidr_block, 1, 1)}"
-}
-
 module "subnet-0" {
   source              = "../gsp-subnet"
   vpc_id              = "${aws_vpc.network.id}"
   cluster_name        = "${var.cluster_name}"
-  private_cidr_block  = "${cidrsubnet(local.private_cidr_block, ceil(log(6, 2)), 0)}"
-  public_cidr_block   = "${cidrsubnet(local.public_cidr_block, ceil(log(6, 2)), 0)}"
   availability_zone   = "eu-west-2a"
   internet_gateway_id = "${aws_internet_gateway.gateway.id}"
+  private_cidr_block  = "10.0.0.0/19"
+  public_cidr_block   = "10.0.32.0/20"
+
+  # protected_cidr_block = "10.0.48.0/21"
+  # spare_cidr_block     = "10.0.56.0/21"
 }
 
 module "subnet-1" {
   source              = "../gsp-subnet"
   vpc_id              = "${aws_vpc.network.id}"
   cluster_name        = "${var.cluster_name}"
-  private_cidr_block  = "${cidrsubnet(local.private_cidr_block, ceil(log(6, 2)), 1)}"
-  public_cidr_block   = "${cidrsubnet(local.public_cidr_block, ceil(log(6, 2)), 1)}"
   availability_zone   = "eu-west-2b"
   internet_gateway_id = "${aws_internet_gateway.gateway.id}"
+  private_cidr_block  = "10.0.64.0/19"
+  public_cidr_block   = "10.0.96.0/20"
+
+  # protected_cidr_block = "10.0.112.0/21"
+  # spare_cidr_block     = "10.0.120.0/21"
 }
 
 module "subnet-2" {
   source              = "../gsp-subnet"
   vpc_id              = "${aws_vpc.network.id}"
   cluster_name        = "${var.cluster_name}"
-  private_cidr_block  = "${cidrsubnet(local.private_cidr_block, ceil(log(6, 2)), 2)}"
-  public_cidr_block   = "${cidrsubnet(local.public_cidr_block, ceil(log(6, 2)), 2)}"
   availability_zone   = "eu-west-2c"
   internet_gateway_id = "${aws_internet_gateway.gateway.id}"
+  private_cidr_block  = "10.0.128.0/19"
+  public_cidr_block   = "10.0.160.0/20"
+
+  # protected_cidr_block = "10.0.176.0/21"
+  # spare_cidr_block     = "10.0.184.0/21"
 }
+
+# following range is left over for future needs
+# spare_subnet_block = "10.0.192.0/18"
+
--- modules/gsp-network/variables.tf
+++ modules/gsp-network/variables.tf
@@ -1,9 +1,3 @@
 variable "cluster_name" {
   type = "string"
 }
-
-variable "cidr_block" {
-  description = "CIDR IPv4 range of the VPC"
-  type        = "string"
-  default     = "10.0.0.0/16"
-}
