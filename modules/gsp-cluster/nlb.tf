0b64a220cc6348b3af11aa25cab16018d0dc7dcb
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-07 18:43:07 +0100

    ingress nlb and target groups
    create target groups for http (40080) and https (40443) traffic and
    wire them to the autoscaling groups as part of the nodegroup
    provisioning.
    
    port 40080 for http and port 40443 were chosen arbitarily.
    
    an NLB is provisioned that forwards traffic to the target groups:
    
     80 -> 40080
    443 -> 40443
    
    static ingress eips are passed through from the network module and
    attached to the NLB , but maybe should be moved to the gsp-cluster
    module where they are consumed.

---
+++ modules/gsp-cluster/nlb.tf
@@ -0,0 +1,56 @@
+data "aws_eip" "ingress-0" {
+  public_ip = "${var.ingress_ips[0]}"
+}
+
+data "aws_eip" "ingress-1" {
+  public_ip = "${var.ingress_ips[1]}"
+}
+
+data "aws_eip" "ingress-2" {
+  public_ip = "${var.ingress_ips[2]}"
+}
+
+resource "aws_lb" "ingress" {
+  name                             = "${var.cluster_name}-ingress"
+  load_balancer_type               = "network"
+  enable_cross_zone_load_balancing = true
+
+  subnet_mapping {
+    subnet_id     = "${var.public_subnet_ids[0]}"
+    allocation_id = "${data.aws_eip.ingress-0.id}"
+  }
+
+  subnet_mapping {
+    subnet_id     = "${var.public_subnet_ids[1]}"
+    allocation_id = "${data.aws_eip.ingress-1.id}"
+  }
+
+  subnet_mapping {
+    subnet_id     = "${var.public_subnet_ids[2]}"
+    allocation_id = "${data.aws_eip.ingress-2.id}"
+  }
+
+  tags = "${map("Name", "${var.cluster_name}-ingress")}"
+}
+
+resource "aws_lb_listener" "ingress-https" {
+  load_balancer_arn = "${aws_lb.ingress.arn}"
+  port              = "443"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${module.k8s-cluster.worker_https_target_group_arn}"
+  }
+}
+
+resource "aws_lb_listener" "ingress-http" {
+  load_balancer_arn = "${aws_lb.ingress.arn}"
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${module.k8s-cluster.worker_http_target_group_arn}"
+  }
+}
