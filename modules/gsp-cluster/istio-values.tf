cbfc0e88fd05e01f7bdf3af14f66222eb0472c63
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-09 15:07:40 +0100

    remove cert-manager
    we are going to use ACM for cluster certificates so no longer need to
    manage cert-manager

--- modules/gsp-cluster/istio-values.tf
+++ modules/gsp-cluster/istio-values.tf
@@ -1,7 +1,5 @@
 data "template_file" "istio-values" {
   template = "${file("${path.module}/data/istio-values.yaml")}"
 
-  vars {
-    cert_manager_role_name = "${aws_iam_role.cert-manager.name}"
-  }
+  vars {}
 }
