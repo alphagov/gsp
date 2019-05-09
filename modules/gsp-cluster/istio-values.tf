0950f762bcec3e5c99bf3c268cf51d3968b72aab
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-09 18:16:45 +0100

    remove redundent istio-values generation

--- modules/gsp-cluster/istio-values.tf
+++
@@ -1,5 +0,0 @@
-data "template_file" "istio-values" {
-  template = "${file("${path.module}/data/istio-values.yaml")}"
-
-  vars {}
-}
--- modules/gsp-cluster/outputs.tf
+++ modules/gsp-cluster/outputs.tf
@@ -2,11 +2,6 @@ output "kubeconfig" {
   value = "${module.k8s-cluster.kubeconfig}"
 }
 
-output "istio-values" {
-  sensitive = true
-  value     = "${data.template_file.istio-values.rendered}"
-}
-
 output "values" {
   sensitive = true
   value     = "${data.template_file.values.rendered}"
