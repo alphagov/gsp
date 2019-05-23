c1de961ab94d7f583074daf7d99309e0ce12c2df
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-05-23 17:20:38 +0100

    add terraform fmt and fix a typo

--- modules/gsp-cluster/vpc-flow-logs.tf
+++ modules/gsp-cluster/vpc-flow-logs.tf
@@ -6,19 +6,16 @@ resource "aws_flow_log" "vpc_flow_log" {
   vpc_id               = "${var.vpc_id}"
 }
 
-
 resource "aws_cloudwatch_log_group" "vpc_flow_log" {
   name              = "${var.cluster_domain}_vpc_flow_log"
   retention_in_days = 30
 }
 
-
 resource "aws_iam_role" "cloudwatch_vpc_flow_log_shipping" {
-  name = "${var.cluster_name}cloudwatch_vpc_flow_log_shipping"
+  name = "${var.cluster_name}_cloudwatch_vpc_flow_log_shipping"
 
   assume_role_policy = "${data.aws_iam_policy_document.cloudwatch_vpc_flow_log_assume_role.json}"
 }
-
 
 resource "aws_iam_policy" "cloudwatch_vpc_flow_log_shipping" {
   name        = "${var.cluster_name}_cloudwatch_vpc_flow_log_shipping"
@@ -27,13 +24,11 @@ resource "aws_iam_policy" "cloudwatch_vpc_flow_log_shipping" {
   policy = "${data.aws_iam_policy_document.cloudwatch_vpc_flow_log.json}"
 }
 
-
 resource "aws_iam_policy_attachment" "cloudwatch_vpc_flow_log_shipping" {
   name       = "${var.cluster_name}_cloudwatch_vpc_flow_log_shipping"
   roles      = ["${aws_iam_role.cloudwatch_vpc_flow_log_shipping.name}"]
   policy_arn = "${aws_iam_policy.cloudwatch_vpc_flow_log_shipping.arn}"
 }
-
 
 data "aws_iam_policy_document" "cloudwatch_vpc_flow_log_assume_role" {
   statement {
@@ -48,7 +43,6 @@ data "aws_iam_policy_document" "cloudwatch_vpc_flow_log_assume_role" {
   }
 }
 
-
 data "aws_iam_policy_document" "cloudwatch_vpc_flow_log" {
   statement {
     actions = [
@@ -58,6 +52,7 @@ data "aws_iam_policy_document" "cloudwatch_vpc_flow_log" {
       "logs:DescribeLogGroups",
       "logs:DescribeLogStreams",
     ]
+
     resources = [
       "*",
     ]
