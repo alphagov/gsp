c8872aff46f39bff89c41d47b4c19287efd7b0b9
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-05-23 15:49:36 +0100

    generate concourse teams for each namespace
    each namespace will get its own concourse team, and access will
    be granted to the github users who have the appropriate roles assigned
    
    example:
    
    a namespace called "sandbox-myapp" in the "sandbox" cluster would get a
    concourse team called "myapp" created.
    
    user with role "sandbox-sre" or "sandbox-myapp-dev" will get
    pipeline-operator role assigned for the concourse team.

--- hack/lint-terraform-values-output.sh
+++ hack/lint-terraform-values-output.sh
@@ -24,30 +24,42 @@ rm -rf output || echo 'ok'
 mkdir -p output
 
 cat << 'EOF' > output/values.yaml
+global:
+  cluster:
+    domain: fake.com
+    name: sandbox
 namespaces:
 - name: sandbox-canary
+  resources: [{"apiVersion":"v1","kind":"Secret","type":"Opaque","metadata":{"name":"ci-deploy-key"},"data":{"private_key":"RklYTUUK"}},{"apiVersion":"v1","kind":"ConfigMap","metadata":{"name":"ci-deploy-key"},"data":{"public_key":"RklYTUUK"}},{"apiVersion":"flux.weave.works/v1beta1","kind":"HelmRelease","metadata":{"name":"canary"},"spec":{"releaseName":"canary","chart":{"git":"{{ .Values.global.canary.repository }}","ref":"master","path":"charts/gsp-canary","verificationKeys":"{{ .Values.global.canary.verificationKeys }}"},"values":{"annotations":{"iam.amazonaws.com/role":"{{ .Values.global.roles.canary }}"},"updater":{"helmChartRepoUrl":"{{ .Values.global.canary.repository }}"}}}}]
+- name: jefferson-canary
   resources: [{"apiVersion":"v1","kind":"Secret","type":"Opaque","metadata":{"name":"ci-deploy-key"},"data":{"private_key":"RklYTUUK"}},{"apiVersion":"v1","kind":"ConfigMap","metadata":{"name":"ci-deploy-key"},"data":{"public_key":"RklYTUUK"}},{"apiVersion":"flux.weave.works/v1beta1","kind":"HelmRelease","metadata":{"name":"canary"},"spec":{"releaseName":"canary","chart":{"git":"{{ .Values.global.canary.repository }}","ref":"master","path":"charts/gsp-canary","verificationKeys":"{{ .Values.global.canary.verificationKeys }}"},"values":{"annotations":{"iam.amazonaws.com/role":"{{ .Values.global.roles.canary }}"},"updater":{"helmChartRepoUrl":"{{ .Values.global.canary.repository }}"}}}}]
 users:
 - name: chris.farmiloe
   email: chris.farmiloe@digital.cabinet-office.gov.uk
-  ARN: arn:aws:iam::622626885786:user/chris.farmiloe@digital.cabinet-office.gov.uk
+  ARN: arn:aws:iam::000000072:user/chris.farmiloe@digital.cabinet-office.gov.uk
   roles:
   - sandbox-admin
-  - sandbox-sre
-  - sandbox-canary-dev
-  roleARN: arn:aws:iam::011571571136:role/farms-user-chris.farmiloe
+  roleARN: arn:aws:iam::000000072:role/chris.farmiloe
+  github: "chrisfarms"
 - name: sam.crang
   email: sam.crang@digital.cabinet-office.gov.uk
-  ARN: arn:aws:iam::622626885786:user/sam.crang@digital.cabinet-office.gov.uk
+  ARN: arn:aws:iam::000000072:user/sam.crang@digital.cabinet-office.gov.uk
   roles:
-  - sandbox-canary-dev
-  roleARN: arn:aws:iam::011571571136:role/farms-user-sam.crang
+  - sandbox-sre
+  roleARN: arn:aws:iam::000000072:role/sam.crang
+  github: "samcrang"
+- name: jeff.jefferson
+  email: jeff.jefferson@digital.cabinet-office.gov.uk
+  ARN: arn:aws:iam::000000072:user/jeff.jefferson@digital.cabinet-office.gov.uk
+  roles:
+  - jefferson-canary-dev
+  roleARN: arn:aws:iam::000000072:role/jeff.jefferson
+  github: "jefferz83"
 EOF
 
 helm template \
 	--output-dir ./output \
 	--namespace fake-system \
-	--values output/values.yaml \
 	--values <(\
 		cat modules/gsp-cluster/data/values.yaml \
 		| sed 's/${admin_role_arns}/[]/' \
@@ -57,5 +69,6 @@ helm template \
 		| sed 's/${bootstrap_role_arns}/[]/' \
 		| sed 's/${concourse_teams}/["org:team"]/' \
 	) \
+	--values output/values.yaml \
 	"charts/gsp-cluster"
 
