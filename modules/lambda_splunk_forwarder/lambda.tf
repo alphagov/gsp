145594590ec82ca3f2e1103c3b19e3dce3707921
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-06-05 13:59:38 +0100

    lower lambda to 128MB and 120s
    raising the RAM limit had no effect on the timeouts we were seeing so
    may as well lower it again.

--- modules/lambda_splunk_forwarder/lambda.tf
+++ modules/lambda_splunk_forwarder/lambda.tf
@@ -6,8 +6,8 @@ resource "aws_lambda_function" "lambda_log_forwarder" {
   role             = "${aws_iam_role.lambda_log_forwarder.arn}"
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.6"
-  timeout          = "180"
-  memory_size      = "512"
+  timeout          = "120"
+  memory_size      = "128"
   description      = "A function to forward logs from AWS to a Splunk HEC using a manual zip of https://github.com/alphagov/cyber-cloudwatch-fluentd-to-hec"
 
   environment {
