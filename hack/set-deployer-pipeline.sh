964d62f84a540698c8a8f141d1fb09d5c73aed23
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-06-12 14:41:57 +0100

    migrate logic from gsp-teams/set-pipeline
    gsp-teams will soon not exist as a central list of cluster, we don't
    want multiple "set-pipeline" scripts in each cluster-config repo and
    so we make the main platform/set-pipeline script compatible with the
    existing clusters by migrating the logic for the initial approvers
    config based on gds-trusted-users

--- hack/set-deployer-pipeline.sh
+++ hack/set-deployer-pipeline.sh
@@ -3,12 +3,35 @@
 set -eu -o pipefail
 
 : "${CLUSTER_CONFIG:?}"
+: "${USER_CONFIGS:?}"
 
 CLUSTER_NAME=$(yq -r '.["cluster-name"]' < ${CLUSTER_CONFIG})
 PIPELINE_NAME=$(yq -r '.["concourse-pipeline-name"]' < ${CLUSTER_CONFIG})
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-echo ""
+if [[ "${CURRENT_BRANCH}" != "master" ]]; then
+	echo "WARNING: current branch is not master!!!"
+	read -p "Are you sure you want to set pipeline with platform-version pinned to ${CURRENT_BRANCH}? " -n 1 -r
+	echo
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+	fi
+fi
+
+approvers="/tmp/deployer-${CLUSTER_NAME}-approvers.yaml"
+echo -n "github-approvers: " > "${approvers}"
+cat ${USER_CONFIGS}/*.yaml \
+	| yq . \
+	| jq -c -s "[.[] | select(.roles[] | select((. == \"${CLUSTER_NAME}-sre\" ) or (. == \"${CLUSTER_NAME}-admin\"))) | .github] | unique" \
+	>> "${approvers}"
+
+trusted="/tmp/deployer-${CLUSTER_NAME}-keys.yaml"
+echo -n "trusted-developer-keys: " > "${trusted}"
+cat ${USER_CONFIGS}/*.yaml \
+	| yq . \
+	| jq -c -s '[ .[].pub ]' \
+	>> "${trusted}"
 
 fly -t cd-gsp sync
 
@@ -17,8 +40,8 @@ fly -t cd-gsp set-pipeline -p "${PIPELINE_NAME}" \
 	--load-vars-from "pipelines/deployer/deployer.defaults.yaml" \
 	--load-vars-from "${CLUSTER_CONFIG}" \
 	--var "platform-version=${CURRENT_BRANCH}" \
-	--var "config-version=${CURRENT_BRANCH}" \
-	--var "users-version=${CURRENT_BRANCH}" \
+	--load-vars-from "${approvers}" \
+	--load-vars-from "${trusted}" \
 	--check-creds
 
 fly -t cd-gsp expose-pipeline -p "${PIPELINE_NAME}"
