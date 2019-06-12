b519ee2e79aaa98f7860941d81cab03de60e2ddb
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-06-12 11:55:30 +0100

    adds ability to disable-updating of pipeline
    sometimes we want a deployer pipeline that does not update while we are
    developing things, this adds a flag to disable updating and updates the
    set-pipeline script to work with it.

--- hack/set-deployer-pipeline.sh
+++ hack/set-deployer-pipeline.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-set -eu
+set -eu -o pipefail
 
-# example of pushing a deployer pipeline for a cluster named CLUSTER_NAME
-# using the config at examples/cluster/sandbox.yaml
-# but overriding some of the vars
-# and setting the git resources to trigger from the current active git branch
+: "${CLUSTER_CONFIG:?}"
 
-PLATFORM_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+CLUSTER_NAME=$(yq -r '.["cluster-name"]' < ${CLUSTER_CONFIG})
+PIPELINE_NAME=$(yq -r '.["concourse-pipeline-name"]' < ${CLUSTER_CONFIG})
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo ""
 
 fly -t cd-gsp sync
 
-fly -t cd-gsp set-pipeline -p "${CLUSTER_NAME}" --config pipelines/deployer/deployer.yaml \
-	--load-vars-from pipelines/examples/clusters/sandbox.yaml \
-	--var cluster-name=${CLUSTER_NAME} \
-	--var cluster-domain=london.${CLUSTER_NAME}.govsvc.uk \
-	--var platform-version=${PLATFORM_BRANCH} \
-	--var config-version=${PLATFORM_BRANCH} \
+fly -t cd-gsp set-pipeline -p "${PIPELINE_NAME}" \
+	--config "pipelines/deployer/deployer.yaml" \
+	--load-vars-from "pipelines/deployer/deployer.defaults.yaml" \
+	--load-vars-from "${CLUSTER_CONFIG}" \
+	--var "platform-version=${CURRENT_BRANCH}" \
+	--var "config-version=${CURRENT_BRANCH}" \
+	--var "users-version=${CURRENT_BRANCH}" \
 	--check-creds
 
-fly -t cd-gsp expose-pipeline -p "${CLUSTER_NAME}"
+fly -t cd-gsp expose-pipeline -p "${PIPELINE_NAME}"
+
--- pipelines/README.md
+++ pipelines/README.md
@@ -3,5 +3,5 @@
 Example for pushing a deployer pipeline to provision a sandbox cluster can be found in `./hack/set-deployer-pipeline.sh`:
 
 ```
-CLUSTER_NAME=farms ./hack/set-deployer-pipeline.sh
+CLUSTER_CONFIG=./pipelines/examples/clusters/sandbox.yaml ./hack/set-deployer-pipeline.sh
 ```
