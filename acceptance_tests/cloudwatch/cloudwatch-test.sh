6646f9dd0036203e431b4802e86d81e5a44e6301
Stephen Ford <11182275+smford@users.noreply.github.com>
2019-06-14 12:20:40 +0100

    Fix script

--- acceptance_tests/cloudwatch/cloudwatch-test.sh
+++ acceptance_tests/cloudwatch/cloudwatch-test.sh
@@ -13,7 +13,7 @@ timeout="${TEST_TIMEOUT:-30}"
 retries="${TEST_RETRIES:-3}"
 ACCOUNT_NAME="portfolio"
 CLUSTER_NAME="portfolio"
-TEST_LOGS_SINCE="$(date --date 'now - 5 minutes' '+%s')"
+TEST_LOGS_SINCE=$(date --date 'now - 5 minutes' '+%s')
 
 echo "accountname: $ACCOUNT_NAME"
 echo "clustername: $CLUSTER_NAME"
@@ -27,7 +27,7 @@ while [ $i -lt $retries ]
 do
   ((i++))
   echo "attempt: $i"
-  LASTSEENLOG=$(aws logs describe-log-streams --log-group-name $CLUSTER_NAME.$ACCOUNT_NAME.govsvc.uk --log-stream-name-prefix "kubernetes.var.log.containers.sonobuoy_heptio-sonobuoy_istio-proxy" | jq ".logStreams[].lastEventTimestamp" | grep -v "null" | sort -urn | head -n1)
+  LASTSEENLOG=$(AWS_REGION=eu-west-2 aws logs describe-log-streams --log-group-name $CLUSTER_NAME.$ACCOUNT_NAME.govsvc.uk --log-stream-name-prefix "kubernetes.var.log.containers.sonobuoy_heptio-sonobuoy_istio-proxy" | jq ".logStreams[].lastEventTimestamp" | grep -v "null" | sort -urn | head -n1)
 
   echo "lastseen: $LASTSEENLOG"
 
