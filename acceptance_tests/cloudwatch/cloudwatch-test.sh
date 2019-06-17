ba992664506367d397906a103e10953ceb3b3551
Stephen Ford <11182275+smford@users.noreply.github.com>
2019-06-17 10:26:50 +0100

    Clean up script

--- acceptance_tests/cloudwatch/cloudwatch-test.sh
+++ acceptance_tests/cloudwatch/cloudwatch-test.sh
@@ -6,31 +6,34 @@ echo "starting cloudwatch test"
 
 #set -xeuf -o pipefail
 
-AWS_DEFAULT_REGION=eu-west-2
-AWS_REGION=eu-west-2
+# AWS_DEFAULT_REGION=eu-west-2
+# AWS_REGION=eu-west-2
 
-timeout="${TEST_TIMEOUT:-30}"
-retries="${TEST_RETRIES:-3}"
-ACCOUNT_NAME="portfolio"
-CLUSTER_NAME="portfolio"
+TIMEOUT="${TEST_TIMEOUT:-30}"
+RETRIES="${TEST_RETRIES:-3}"
+FARBACK="${TEST_FARBACK:-300}"
+#ACCOUNT_NAME="portfolio"
+#CLUSTER_NAME="portfolio"
 TEST_LOGS_SINCE=$(date '+%s')
 echo "Current time: $TEST_LOGS_SINCE"
-TEST_LOGS_SINCE=$(($TEST_LOGS_SINCE - 300))
-echo "  5 Mins ago: $TEST_LOGS_SINCE"
+TEST_LOGS_SINCE=$(($TEST_LOGS_SINCE - $FARBACK))
+echo "$FARBACK Secs ago: $TEST_LOGS_SINCE"
 
 echo "accountname: $ACCOUNT_NAME"
 echo "clustername: $CLUSTER_NAME"
-echo "timeout: $timeout"
-echo "retries: $retries"
+echo "timeout: $TIMEOUT"
+echo "retries: $RETRIES"
 echo "testlogsince: $TEST_LOGS_SINCE"
 
 
 i=0
-while [ $i -lt $retries ]
+while [ $i -lt $RETRIES ]
 do
   ((i++))
-  echo "attempt: $i"
-  LASTSEENLOG=$(AWS_DEFAULT_REGION=eu-west-2 AWS_REGION=eu-west-2 aws logs describe-log-streams --log-group-name $CLUSTER_NAME.$ACCOUNT_NAME.govsvc.uk --log-stream-name-prefix "kubernetes.var.log.containers.sonobuoy_heptio-sonobuoy_istio-proxy" | jq ".logStreams[].lastEventTimestamp" | grep -v "null" | sort -urn | head -n1)
+  echo "Attempt: $i"
+  # LASTSEENLOG=$(AWS_DEFAULT_REGION=eu-west-2 AWS_REGION=eu-west-2 aws logs describe-log-streams --log-group-name $CLUSTER_NAME.$ACCOUNT_NAME.govsvc.uk --log-stream-name-prefix "kubernetes.var.log.containers.sonobuoy_heptio-sonobuoy_istio-proxy" | jq ".logStreams[].lastEventTimestamp" | grep -v "null" | sort -urn | head -n1)
+
+  LASTSEENLOG=$(AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION AWS_REGION=$AWS_REGION aws logs describe-log-streams --log-group-name $CLUSTER_NAME.$ACCOUNT_NAME.govsvc.uk --log-stream-name-prefix "kubernetes.var.log.containers.sonobuoy_heptio-sonobuoy_istio-proxy" | jq ".logStreams[].lastEventTimestamp" | grep -v "null" | sort -urn | head -n1)
 
   echo "lastseen: $LASTSEENLOG"
 
@@ -39,7 +42,7 @@ do
     exit 0
   fi
 
-  sleep ${timeout}
+  sleep ${TIMEOUT}
 done
 
 echo "FAIL: No logs have been detected reaching cloudwatch since $TEST_LOGS_SINCE" 2>&1 | tee /tmp/results
