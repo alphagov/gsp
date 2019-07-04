40c31a17900a7a81ed246d338c4414f902baf202
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-07-04 11:03:36 +0100

    update deployer to read from releases

--- hack/set-deployer-pipeline.sh
+++ hack/set-deployer-pipeline.sh
@@ -3,35 +3,12 @@
 set -eu -o pipefail
 
 : "${CLUSTER_CONFIG:?}"
-: "${USER_CONFIGS:?}"
 
 CLUSTER_NAME=$(yq -r '.["cluster-name"]' < ${CLUSTER_CONFIG})
 PIPELINE_NAME=$(yq -r '.["concourse-pipeline-name"]' < ${CLUSTER_CONFIG})
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [[ "${CURRENT_BRANCH}" != "master" ]]; then
-	echo "WARNING: current branch is not master!!!"
-	read -p "Are you sure you want to set pipeline with platform-version pinned to ${CURRENT_BRANCH}? " -n 1 -r
-	echo
-	if [[ ! $REPLY =~ ^[Yy]$ ]]
-	then
-		[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-	fi
-fi
 
 echo "generating approvers for ${CLUSTER_NAME}..."
 
-approvers="/tmp/deployer-${CLUSTER_NAME}-approvers.yaml"
-echo -n "github-approvers: " > "${approvers}"
-yq . ${USER_CONFIGS}/*.yaml \
-	| jq -c -s "[.[] | select(.roles[] | select((. == \"${CLUSTER_NAME}-sre\" ) or (. == \"${CLUSTER_NAME}-admin\"))) | .github] | unique | sort" \
-	>> "${approvers}"
-
-trusted="/tmp/deployer-${CLUSTER_NAME}-keys.yaml"
-echo -n "trusted-developer-keys: " > "${trusted}"
-yq . ${USER_CONFIGS}/*.yaml \
-	| jq -c -s '[ .[].pub ] | sort' \
-	>> "${trusted}"
 
 fly -t cd-gsp sync
 
@@ -39,9 +16,8 @@ fly -t cd-gsp set-pipeline -p "${PIPELINE_NAME}" \
 	--config "pipelines/deployer/deployer.yaml" \
 	--load-vars-from "pipelines/deployer/deployer.defaults.yaml" \
 	--load-vars-from "${CLUSTER_CONFIG}" \
-	--var "platform-version=${CURRENT_BRANCH}" \
-	--load-vars-from "${approvers}" \
-	--load-vars-from "${trusted}" \
+	--yaml-var 'github-approvers=[]' \
+	--yaml-var 'trusted-developer-keys=[]' \
 	--check-creds "$@"
 
 fly -t cd-gsp expose-pipeline -p "${PIPELINE_NAME}"
