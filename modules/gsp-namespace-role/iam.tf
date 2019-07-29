260c343f0b6e43044a866bd8cb26907f484cdf43
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2019-07-29 13:57:15 +0100

    Add role for each namespace that will allow it to access specifically prefixes AWS resources
    Currently just the SQS Queues people might want to create with AWS Service Operator.

---
+++ modules/gsp-namespace-role/iam.tf
@@ -0,0 +1,39 @@
+data "aws_iam_policy_document" "assume-role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals = {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:role/${var.cluster_name}_kiam_server"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "namespace-defaults" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+
+    resources = ["arn:aws:sqs:*:${var.account_id}:${var.cluster_name}-sqsqueue-${var.namespace_name}-*"]
+  }
+}
+
+resource "aws_iam_role" "namespace" {
+  name               = "${var.cluster_name}-namespace-${var.namespace_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume-role.json}"
+}
+
+resource "aws_iam_policy" "namespace-defaults" {
+  name   = "${var.cluster_name}-namespace-${var.namespace_name}-defaults"
+  policy = "${data.aws_iam_policy_document.namespace-defaults.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "namespace-defaults" {
+  role       = "${aws_iam_role.namespace.name}"
+  policy_arn = "${aws_iam_policy.namespace-defaults.arn}"
+}
---
+++ modules/gsp-namespace-role/variables.tf
@@ -0,0 +1,11 @@
+variable "namespace_name" {
+  description = "unique name for the namespace"
+}
+
+variable "cluster_name" {
+  description = "cluster name to scope this role to"
+}
+
+variable "account_id" {
+  description = "account ID this cluster is running in"
+}
