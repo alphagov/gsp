a7271995103a2a446db704e8041a9d92d10562a3
Issy Long <me@issyl0.co.uk>
2019-08-05 14:17:31 +0100

    kubebuilder create api --group database --version v1beta1 --kind Postgres
    - This generates a load of boilerplate.
    - We thought about calling this "service", but that's overloaded.
    - We can have "database" and "queue" groups instead.

--- components/service-operator/main.go
+++ components/service-operator/main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	databasev1beta1 "github.com/alphagov/gsp/components/service-operator/api/v1beta1"
+	"github.com/alphagov/gsp/components/service-operator/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = databasev1beta1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +61,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.PostgresReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Postgres"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Postgres")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
