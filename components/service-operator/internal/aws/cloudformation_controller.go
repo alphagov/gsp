3c059047321cd7f70e0ff8f83b8dd6ccf11371e8
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2019-08-16 11:47:09 +0100

    fixup! Refactor AWSReconciler to Controller

--- components/service-operator/internal/aws/cloudformation_controller.go
+++ components/service-operator/internal/aws/cloudformation_controller.go
@@ -56,7 +56,7 @@ var (
 )
 
 func (r *CloudFormationController) Reconcile(log logr.Logger, ctx context.Context, req ctrl.Request, cloudFormationTemplate CloudFormationTemplate, deleting bool) (internal.Action, StackData, error) {
-	stackName := fmt.Sprintf("%s-%s-%s-%s-%s", r.ClusterName, "gsp-service-operator", cloudFormationTemplate.ResourceType(), req.NamespacedName.Namespace, req.NamespacedName.Name)
+	stackName := fmt.Sprintf("%s-%s-%s-%s-%s", r.ClusterName, "gsp-service-operator", cloudFormationTemplate.ResourceType(), req.Namespace, req.Name)
 	// secretName := coalesceString(postgres.Spec.Secret, postgres.Name)
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
