ffc45230077453fd0caee4136e22b61fd7c8b1f0
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2019-10-15 15:59:14 +0100

    go mod tidy
    update deps and ensure that ginkgo and controller-gen are fetched ahead
    of time

--- components/service-operator/tools/tools.go
+++ components/service-operator/tools/tools.go
@@ -4,6 +4,8 @@ package tools
 
 import (
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
+	_ "github.com/onsi/ginkgo"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
 
 // This file imports packages that are used when running go generate, or used
