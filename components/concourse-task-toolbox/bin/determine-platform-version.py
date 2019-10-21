eee086670ae20cd02a6e284aed64fe633793722d
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2019-10-21 17:18:10 +0100

    Obsolete full-gsp through cunning use of repositories

--- components/concourse-task-toolbox/bin/determine-platform-version.py
+++ components/concourse-task-toolbox/bin/determine-platform-version.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import sys
 
-os.mkdirs("platform-version", exist_ok=True)
+os.makedirs("platform-version", exist_ok=True)
 
 print("Picking platform version...")
 partial_repos = [
@@ -14,23 +14,31 @@ partial_repos = [
     "concourse-github-resource-source",
     "concourse-harbor-resource-source"
 ]
-hashes = set(map(lambda r: open(f"{r}/.git/ref").read(), partial_repos))
+commits_known_by = {open(f"{r}/.git/ref").read(): [] for r in partial_repos}
 
-proc = subprocess.Popen(
-    ['git', 'log', '--format=%H'],
-    env={'GIT_DIR': 'full-gsp/.git'},
-    stdout=subprocess.PIPE
-)
-while True:
-    line = proc.stdout.readline().decode('ascii')
-    if not line:
-        sys.stderr.write("Did not get line when running git-log through full-gsp\n")
-        sys.exit(1)
-    if line in hashes:
-        proc.terminate()
-        break
+for partial_repo in partial_repos:
+    proc = subprocess.Popen(
+        ['git', 'log', '--format=%H'],
+        env={'GIT_DIR': f'{partial_repo}/.git'},
+        stdout=subprocess.PIPE
+    )
+    seen_commits = set()
+    while len(seen_commits) < len(list(commits_known_by.keys())):
+        line = proc.stdout.readline().decode('ascii')
+        if not line:
+            break
+        if line in commits_known_by:
+            seen_commits.update([line])
+            commits_known_by[line].append(partial_repo)
 
-line = line.strip()
-print(f"Picked {line}")
+# Pick the only commit which has one repo knowing about it
+obscure_commits = list(filter(lambda i: len(i[1]) == 1, commits_known_by.items()))
+if len(obscure_commits) != 1:
+    sys.stderr.write(f"More or less than one commit was found in a single repository\n")
+    sys.exit(1)
+
+(commit, _), = obscure_commits
+
+print(f"Picked {commit}")
 with open('platform-version/ref', 'w') as f:
-    f.write(line)
+    f.write(commit)
