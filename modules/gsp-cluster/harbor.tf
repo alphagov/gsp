8ed6d40eaa1471adfc6425e3c329f1df87beafe6
Sam Crang <sam.crang@gmail.com>
2020-01-10 11:30:53 +0000

    Apply RDS changes immediately
    This allows us to apply the upcoming RDS Root CA certificate rotation[1]
    during office hours.
    
    This also means that we'll hopefully catch changes that would break
    clusters faster rather than finding out during the maintenance window
    (which is probably during the night sometime).
    
    [1] https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/UsingWithRDS.SSL-certificate-rotation.html

--- modules/gsp-cluster/harbor.tf
+++ modules/gsp-cluster/harbor.tf
@@ -161,6 +161,7 @@ resource "aws_rds_cluster_instance" "harbor" {
   cluster_identifier   = aws_rds_cluster.harbor.id
   engine               = "aurora-postgresql"
   engine_version       = "10.7"
+  apply_immediately    = true
   instance_class       = "db.t3.medium"
   db_subnet_group_name = aws_db_subnet_group.private.name
 
