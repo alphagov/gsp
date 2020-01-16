9520506bdb8a9633776e6032285cbeedb7be4984
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-01-16 14:18:08 +0000

    stop dataplane, host, legacy log ship to splunk
    The (legacy) "pods" log group is no-longer used, and this is just dead
    code while we waited for the transition from "pods" -> "application" log
    group... so remove it!
    
    The "database" and "host" log groups are mostly unstructured logs that
    are currently not used in any splunk protective monitoring and so is
    just contributing to increased log volume costs and noise in splunk
    queries .... so remove them too!
    
    "application" (pod logs) and "eks" (audit/schduler/api logs) required in
    many protective monitoring use-cases continue to be shipped unaffected.

--- modules/gsp-cluster/splunk-system.tf
+++ modules/gsp-cluster/splunk-system.tf
@@ -1,22 +1,3 @@
-module "k8s_lambda_splunk_forwarder" {
-  source                    = "../lambda_splunk_forwarder"
-  enabled                   = var.splunk_enabled
-  name                      = "pods"
-  cloudwatch_log_group_arn  = aws_cloudwatch_log_group.logs.arn
-  cloudwatch_log_group_name = aws_cloudwatch_log_group.logs.name
-  cluster_name              = var.cluster_name
-  splunk_hec_token          = var.k8s_splunk_hec_token
-  splunk_hec_url            = var.splunk_hec_url
-  splunk_index              = var.k8s_splunk_index
-}
-resource "aws_cloudwatch_log_subscription_filter" "legacy_logs" {
-  count           = var.cls_destination_enabled == "1" ? 1 : 0
-  name            = "legacy_logs"
-  log_group_name  = aws_cloudwatch_log_group.logs.name
-  filter_pattern  = ""
-  destination_arn = var.cls_destination_arn
-}
-
 module "k8s_app_lambda_splunk_forwarder" {
   source                    = "../lambda_splunk_forwarder"
   enabled                   = var.splunk_enabled
@@ -32,44 +13,6 @@ resource "aws_cloudwatch_log_subscription_filter" "application_logs" {
   count           = var.cls_destination_enabled == "1" ? 1 : 0
   name            = "application_logs"
   log_group_name  = aws_cloudwatch_log_group.application_logs.name
-  filter_pattern  = ""
-  destination_arn = var.cls_destination_arn
-}
-
-module "k8s_host_lambda_splunk_forwarder" {
-  source                    = "../lambda_splunk_forwarder"
-  enabled                   = var.splunk_enabled
-  name                      = "host"
-  cloudwatch_log_group_arn  = aws_cloudwatch_log_group.host_logs.arn
-  cloudwatch_log_group_name = aws_cloudwatch_log_group.host_logs.name
-  cluster_name              = var.cluster_name
-  splunk_hec_token          = var.k8s_splunk_hec_token
-  splunk_hec_url            = var.splunk_hec_url
-  splunk_index              = var.k8s_splunk_index
-}
-resource "aws_cloudwatch_log_subscription_filter" "host_logs" {
-  count           = var.cls_destination_enabled == "1" ? 1 : 0
-  name            = "host_logs"
-  log_group_name  = aws_cloudwatch_log_group.host_logs.name
-  filter_pattern  = ""
-  destination_arn = var.cls_destination_arn
-}
-
-module "k8s_dataplane_lambda_splunk_forwarder" {
-  source                    = "../lambda_splunk_forwarder"
-  enabled                   = var.splunk_enabled
-  name                      = "dataplane"
-  cloudwatch_log_group_arn  = aws_cloudwatch_log_group.dataplane_logs.arn
-  cloudwatch_log_group_name = aws_cloudwatch_log_group.dataplane_logs.name
-  cluster_name              = var.cluster_name
-  splunk_hec_token          = var.k8s_splunk_hec_token
-  splunk_hec_url            = var.splunk_hec_url
-  splunk_index              = var.k8s_splunk_index
-}
-resource "aws_cloudwatch_log_subscription_filter" "dataplane_logs" {
-  count           = var.cls_destination_enabled == "1" ? 1 : 0
-  name            = "dataplane_logs"
-  log_group_name  = aws_cloudwatch_log_group.dataplane_logs.name
   filter_pattern  = ""
   destination_arn = var.cls_destination_arn
 }
