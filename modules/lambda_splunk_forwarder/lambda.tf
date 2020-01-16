6b9623a4b2af04fc5ff822c51bdf351d99849e9f
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2020-01-16 14:12:53 +0000

    update log shipping lambda
    We updated the cycber-cloudwatch-fludentd-to-hec lambda to improve error
    handling and make it more tolerant of logs/timestamps it couldn't
    previously understand.
    
    zip built from this commit: https://github.com/alphagov/cyber-cloudwatch-fluentd-to-hec/pull/18/commits/894329c99b062770bbc1abe42224736bedaf5e56
    
    bump to python 3.7 runtime, and increase timeout to 4mins

--- modules/lambda_splunk_forwarder/lambda.tf
+++ modules/lambda_splunk_forwarder/lambda.tf
@@ -6,8 +6,8 @@ resource "aws_lambda_function" "lambda_log_forwarder" {
 
   role        = "${aws_iam_role.lambda_log_forwarder[0].arn}"
   handler     = "lambda_function.lambda_handler"
-  runtime     = "python3.6"
-  timeout     = "120"
+  runtime     = "python3.7"
+  timeout     = "240"
   memory_size = "128"
   description = "A function to forward logs from AWS to a Splunk HEC using a manual zip of https://github.com/alphagov/cyber-cloudwatch-fluentd-to-hec"
 
