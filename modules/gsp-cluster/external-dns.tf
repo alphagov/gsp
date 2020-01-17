caf3be7a766e6950405fb989bd4a8c2c1438adff
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2020-01-17 15:45:22 +0000

    Switch external-dns role to use IAM roles for EKS service accounts

--- modules/gsp-cluster/external-dns.tf
+++ modules/gsp-cluster/external-dns.tf
@@ -1,3 +1,21 @@
+data "aws_iam_policy_document" "trust_external_dns" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [module.k8s-cluster.oidc_provider_arn]
+    }
+
+    condition {
+      test = "StringEquals"
+      variable = "${replace(module.k8s-cluster.oidc_provider_url, "https://", "")}:sub"
+      values = ["system:serviceaccount:gsp-system:gsp-external-dns"]
+    }
+  }
+}
+
 data "aws_iam_policy_document" "external_dns" {
   statement {
     effect = "Allow"
@@ -35,7 +53,7 @@ resource "aws_iam_policy" "external_dns" {
 resource "aws_iam_role" "external_dns" {
   name = "${var.cluster_name}_external_dns"
 
-  assume_role_policy = data.aws_iam_policy_document.trust_kiam_server.json
+  assume_role_policy = data.aws_iam_policy_document.trust_external_dns.json
 }
 
 resource "aws_iam_policy_attachment" "external_dns" {
